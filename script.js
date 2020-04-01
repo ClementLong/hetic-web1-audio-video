b4b4811ce2948e90b608d6d32c316208f4d6cb3a
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-04-01 11:47:49 +0200

    add some design

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const audio = document.querySelector(".audio");
 const play = document.querySelector(".play");
+const muted = document.querySelector(".muted");
+const volume = document.querySelector(".volume");
 
 play.addEventListener("click", () => {
   if (audio.paused) {
@@ -7,4 +9,18 @@ play.addEventListener("click", () => {
   } else {
     audio.pause();
   }
+});
+
+muted.addEventListener("click", () => {
+  if (audio.muted) {
+    audio.muted = false;
+    muted.innerHTML = "Mute";
+  } else {
+    audio.muted = true;
+    muted.innerHTML = "Unmute";
+  }
+});
+
+volume.addEventListener("change", () => {
+  audio.volume = volume.value / 100;
 });
